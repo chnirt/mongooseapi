71ac2907efe32b87b44635d95b5ac660a203b5d8
Chin <35022967+chinnghiemtuc@users.noreply.github.com>
2019-03-11 14:17:42 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,21 +1,25 @@
-// const http = require('http')
-// const app = require('./app')
+const http = require('http')
+const app = require('./app')
 
-// const port = process.env.PORT || 6969
+const port = process.env.PORT || 6969
 
-// const server = http.createServer(app)
+const server = http.createServer(app, (req, res) => {
+	res.send(JSON.stringify({ Hello: 'World' }))
+})
 
-// server.listen(port, () => console.log(`Listening on ${ port }`))
+server.listen(port, () => console.log(`Listening on ${ port }`))
 
-var express = require('express');
-var port = process.env.PORT || 3000;
-var app = express();
-app.get('/', function (req, res) {
-	res.send(JSON.stringify({ Hello: 'World' }));
-});
-app.listen(port, function () {
-	console.log('Example app listening on port !');
-});
+// var express = require('express');
+// var port = process.env.PORT || 3000;
+// var app = express();
+// app.get('/', function (req, res) {
+// 	res.send(JSON.stringify({ Hello: 'World' }));
+// });
+// app.listen(port, function () {
+// 	console.log('Example app listening on port !');
+// });
+
+
 
 // const express = require('express');
 
